d3d9db94288694381f4fd9a3a2b1cc9e40e7e3fb
João Gabriel Reis Araújo <132390174+MeJohnGabriel@users.noreply.github.com>
2025-02-24 20:15:25 -0300

    restructuring nested arrays and objects

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -29,17 +29,76 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+  orderDelivery: function (obj) {
+    console.log(obj);
+  },
   // MY PRACTICE
   locationSel: function (catIndex) {
     return [this.categories[catIndex]];
   },
 };
-// MY PRACTICE
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 11',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+// ------ DESTRUCTURING OBJECTS
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 99;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
+/*
+<---- you can declare variables ------>
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o,c );
+*/
+
+// ------IF WE WANT TO CHANGE THE VARIABLES NAME
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+/* -----DESTRUCTURING ARRAYS >>>> MY PRACTICE
 const [begin] = restaurant.locationSel(0);
 console.log(begin);
 // HOW to receive two return values from a function
 const [starter, main] = restaurant.order(2, 0);
 console.log(starter, main);
+
+// NESTED DESTRUCTURING
+const nested_0 = [3, 4, [5, 6]];
+const [l, , m] = nested_0;
+console.log(l, m);
+
+const nested = [2, 5, [6, 7]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// DEFAULT VALUES
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
 
 /*-------- W/OUT DESTRUCTURING
 const arr = [2, 3, 4];
@@ -50,6 +109,7 @@ console.log(a, b, c);
 
 /-------- W/ DESTRUCTURING
 const [x, y, z] = arr;
+console.log(x, y, z);
 
 /*
 let [main, , secondary] = restaurant.categories;
