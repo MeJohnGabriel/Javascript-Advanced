d71f06e3fd0f422704cbae8e4bc387cc607b9df0
João Gabriel Reis Araújo <132390174+MeJohnGabriel@users.noreply.github.com>
2025-03-20 11:23:38 -0300

    updates
    SUMMARY :  DESTRUCTURING , SPREAD OPERATOR, REST OPERATOR , OR and AND OPERATOR , NULLISH OPERATOR , LOGICAL ASSIGNMENT OPERATORS

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
-// SUMMARY :  DESTRUCTURING , SPREAD OPERATOR, REST OPERATOR
+// SUMMARY :  DESTRUCTURING , SPREAD OPERATOR, REST OPERATOR , OR and AND OPERATOR , NULLISH OPERATOR , LOGICAL ASSIGNMENT OPERATORS
 // Data needed for a later exercise
+
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
@@ -66,7 +67,68 @@ const restaurant = {
     );
   },
 };
-// SPREAD IS DONE ON THE RIGHT SIDE OF THE ASSIGN OPERATOR =
+
+/*
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Pizz',
+  owner: 'Giovaani',
+};
+
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// ------ LOGICAL 'OR' ASSIGNMENT OPERATOR
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+//Nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// ------ LOGICAL 'AND' ASSIGNMENT OPERATOR
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+console.log(rest1);
+console.log(rest2);
+
+
+// NULLISH COALESCING OPERATOR
+restaurant.numGuests = 0;
+const guest2 = restaurant.numGuests || 10;
+console.log(guest2);
+
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+// ----- OR and AND Operators 
+console.log(`----OR----`);
+console.log(3 || 'John');
+console.log('' || 0);
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+
+const guest1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guest1);
+
+console.log(`----AND----`);
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+
+//------SPREAD IS DONE ON THE RIGHT SIDE OF THE ASSIGN OPERATOR =
 // Destructuring
 const arr = [1, 2, ...[3, 4]];
 
@@ -100,9 +162,25 @@ add(3, 4, 6, 7);
 add(3, 4, 6, 7, 9, 10);
 const x = [23, 5, 2];
 add(...x);
-
+// my practice
+const addEnhanced = function (...values) {
+  let add = 0;
+  let str = '';
+  for (let i = 0; i < values.length; i++) {
+    add = add + values[i];
+    str += ` + ${values[i]}`;
+  }
+  console.log(`The total of ${str} is: ${add}`);
+};
+addEnhanced(2, 3, 4);
 restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
-
+//my pracice function that takes an array as an argument
+const arrFunction = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+arrFunction([3, 2, 3]);
 /*My practice 54 (spread)
 const drinkSelection = [
   prompt('How many drinks?'),
@@ -268,3 +346,128 @@ console.log(letters);
 console.log(...str);
 // console.log(`${...str} Schmedtmann`) THIS DOESNT WORK CAUSE THE '${}' DOESNT WORK W/ MULTIPLE VALUES SEPARATED BY A COMMA
 */
+
+// >>>>>>>>CHALENGES<<<<<<<<<<<<
+
+/* 
+DESCRIPTION: 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+
+console.log(`Bayer: ${player1}.`);
+console.log(`Borussia: ${player2}.`);
+
+// 1 and 2:
+console.log('---Bayern Munich---');
+const [player1, player2] = game.players;
+const [gk, ...fieldPlayers] = player1;
+console.log(`Goalkeeper: ${gk}`);
+console.log(`Field players:`, ...fieldPlayers);
+
+console.log('---Borrussia Dortmund---');
+const [gk2, ...fieldPlayers2] = player2;
+console.log(`Goalkeeper: ${gk2}`);
+console.log(`Field players:`, ...fieldPlayers2);
+
+//3
+const allPlayers = [...player1, ...player2];
+console.log('||| All players ||| : ', ...allPlayers);
+
+//4
+console.log('---Substitute players---');
+const players1Final = [...player1, 'Tiago', 'Coutinho', 'Harry'];
+console.log(...players1Final);
+
+//5
+console.log('---Game odds---');
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+
+//6
+console.log('---Selecting player(s)---');
+
+const printGoals = function (...players) {
+  console.log(
+    `Players that scored:`,
+    ...players,
+    `| Total goals:`,
+    players.length
+  );
+};
+printGoals('Davies', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+// 7
+
+// team1: 1.33,
+// x: 3.25,
+// team2: 6.5,
+
+const printOds = function () {
+  team1 < team2 && console.log(`Team 1 (odds: ${team1}) is more likely to win`);
+  team1 > team2 && console.log(`Team 2 (odds: ${team2}) is more likely to win`);
+};
+printOds();
+*/
+
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+for (const item of menu) console.log(item);
+
+for (let i = 0; i < menu.length; i++) {
+  console.log(menu[i]);
+}
